985a0042264a44d48c81847ff700b61e35b53cdb
mirarodd548 <mirarodd548@SNS-D-130S00>
2020-01-15 13:08:55 -0500

    Add project files.

---
+++ DodgeGame/Screens/GameScreen.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace DodgeGame
+{
+    partial class GameScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.Scorelable = new System.Windows.Forms.Label();
+            this.scorVarible = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(207, 238);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 13);
+            this.outputLabel.TabIndex = 0;
+            // 
+            // Scorelable
+            // 
+            this.Scorelable.AutoSize = true;
+            this.Scorelable.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Scorelable.Location = new System.Drawing.Point(3, 0);
+            this.Scorelable.Name = "Scorelable";
+            this.Scorelable.Size = new System.Drawing.Size(98, 36);
+            this.Scorelable.TabIndex = 1;
+            this.Scorelable.Text = "Score:";
+            // 
+            // scorVarible
+            // 
+            this.scorVarible.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorVarible.Location = new System.Drawing.Point(97, 0);
+            this.scorVarible.Name = "scorVarible";
+            this.scorVarible.Size = new System.Drawing.Size(217, 36);
+            this.scorVarible.TabIndex = 2;
+            // 
+            // GameScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.Controls.Add(this.scorVarible);
+            this.Controls.Add(this.Scorelable);
+            this.Controls.Add(this.outputLabel);
+            this.DoubleBuffered = true;
+            this.Name = "GameScreen";
+            this.Size = new System.Drawing.Size(327, 305);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.GameScreen_Paint);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.GameScreen_KeyUp);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.GameScreen_PreviewKeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label Scorelable;
+        private System.Windows.Forms.Label scorVarible;
+    }
+}
